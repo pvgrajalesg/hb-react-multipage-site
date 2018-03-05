16477fe5db2477eb2ec3eeb1ba00ede7ae0e3d0c
pvgrajalesg <pvgrajalesg@unal.edu.co>
2018-03-04 21:41:33 -0500

    changing data user

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,7 +4,7 @@ import './Message.scss'
 export default function Message ({user}) {
   return (
     <div className='message'>
-      { 'Welcome ' + user.nombre + ' ' + user.apellido }
+      { 'Welcome ' + user.name + ' ' + user.lastName }
     </div>
   )
 }
