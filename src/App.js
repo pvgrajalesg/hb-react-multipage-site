deca051ea8fa74b4f214a36b5a779752aaf50b46
pvgrajalesg <pvgrajalesg@unal.edu.co>
2018-03-08 11:21:45 -0500

    changing class App to function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React, { Component, Fragment} from 'react'
+import React, { Fragment} from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 import {user} from './data/home.json'
 import Message from './components/Message'
 
-export default class App extends Component {
-	constructor(props){
-		super(props)
-	}
-
-  render () {
-    return (
-    	<Router>
-    		<Fragment>
-    			<Route exact path='/'
-      			render={() => <Message user={user} />} />
+export default function App {
+  return (
+    <Router>
+    	<Fragment>
+    		<Route exact path='/' render={() => <Message user={user} />} />
       	</Fragment>
-      </Router>
+     </Router>
     )
-  }
 }
